259086677872e17a3108c84e03bbb875ad8d5d38
Anne-15 <wariiyuanne@gmail.com>
2023-10-17 09:21:50 +0300

    adding .gitignore

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import ssl
 from flask import Flask, render_template, request
 import os
 from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-gmail_user = os.getenv('gmail_user')
-gmail_password = os.getenv('gmail_password')
+gmail_user = os.getenv('GMAIL_USER')
+gmail_password = os.getenv('GMAIL_PASSWORD')
 
 # configuring values
 app.config.update(
@@ -29,10 +32,10 @@ def contact():
         subject = request.form.get('subject')
         message = request.form.get('message')
 
-        print("Name:", name)
-        print("Email:", email)
-        print("Subject:", subject)
-        print("Message:", message)
+        # print("Name:", name)
+        # print("Email:", email)
+        # print("Subject:", subject)
+        # print("Message:", message)
 
         mail.send_message("Message from " + name + " at " + email,
                         sender = email,
